6404b98bdd848a677c34df73a6f0ed090cc613a6
刘豪强 <122520213@qq.com>
2021-09-25 21:17:51 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an ab111out pa11111ge</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+  <el-button type="text" @click="open">点击打开 Message Box</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods:{
+      open() {
+        console.log(111)
+        this.$confirm('确定要结束当前会话吗？', '结束会话确认', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+        }).then(() => {
+        this.$confirm('您已退出当前会话，感谢您的咨询，请对我的服务做出评价', {
+          confirmButtonText: '评价',
+          center: true,
+          showClose:false,
+          closeOnClickModal:false,
+          showCancelButton:false
+        }).then(() => {
+          console.log('成功')
+        })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      }
+  }
+}
+</script>
